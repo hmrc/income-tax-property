e1703dca5686e323443fe5e28e0c3e3facab915c
hmrc-web-operations <hmrc-web-operations@digital.hmrc.gov.uk>
2023-07-19 10:54:32 +0000

    'Creating new service income-tax-property'

---
+++ app/uk/gov/hmrc/incometaxproperty/config/Module.scala
@@ -0,0 +1,11 @@
+package uk.gov.hmrc.incometaxproperty.config
+
+import com.google.inject.AbstractModule
+
+class Module extends AbstractModule {
+
+  override def configure(): Unit = {
+
+    bind(classOf[AppConfig]).asEagerSingleton()
+  }
+}
