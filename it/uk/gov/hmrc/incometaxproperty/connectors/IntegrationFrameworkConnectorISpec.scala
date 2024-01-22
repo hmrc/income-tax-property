12a558ba748d54e6e8627312edf0a51350d014d8
adam-shahzad-2 <adamshahzad343@gmail.com>
2024-01-22 08:55:35 +0000

    quick fixes

--- it/uk/gov/hmrc/incometaxproperty/connectors/IntegrationFrameworkConnectorISpec.scala
+++ it/uk/gov/hmrc/incometaxproperty/connectors/IntegrationFrameworkConnectorISpec.scala
@@ -239,7 +239,7 @@ class IntegrationFrameworkConnectorISpec extends ConnectorIntegrationTest
         val httpResponse = HttpResponse(CONFLICT, Json.toJson(SingleErrorBody("some-code", "Conflict")).toString())
         val taxYear = 2024
 
-        stubPutHttpClientCall(s"/income-tax/business/property/periodic/23-24\\?taxableEntityId=$nino&incomeSourceId=$incomeSourceId",  Json.toJson(aPropertyAnnualSubmission).toString(), httpResponse)
+        stubPutHttpClientCall(s"/income-tax/business/property/annual/23-24\\?taxableEntityId=$nino&incomeSourceId=$incomeSourceId",  Json.toJson(aPropertyAnnualSubmission).toString(), httpResponse)
 
         await(underTest.createOrUpdateAnnualSubmission(taxYear, nino, incomeSourceId,  Json.toJson(aPropertyAnnualSubmission))(hc)) shouldBe Left(ApiError(CONFLICT, SingleErrorBody("some-code", "Conflict")))
       }
@@ -247,7 +247,7 @@ class IntegrationFrameworkConnectorISpec extends ConnectorIntegrationTest
         val httpResponse = HttpResponse(NOT_FOUND, Json.toJson(SingleErrorBody("some-code", "NotFound")).toString())
         val taxYear = 2024
 
-        stubPutHttpClientCall(s"/income-tax/business/property/periodic/23-24\\?taxableEntityId=$nino&incomeSourceId=$incomeSourceId",  Json.toJson(aPropertyAnnualSubmission).toString(), httpResponse)
+        stubPutHttpClientCall(s"/income-tax/business/property/annual/23-24\\?taxableEntityId=$nino&incomeSourceId=$incomeSourceId",  Json.toJson(aPropertyAnnualSubmission).toString(), httpResponse)
 
         await(underTest.createOrUpdateAnnualSubmission(taxYear, nino, incomeSourceId,  Json.toJson(aPropertyAnnualSubmission))(hc)) shouldBe Left(ApiError(NOT_FOUND, SingleErrorBody("some-code", "NotFound")))
       }
