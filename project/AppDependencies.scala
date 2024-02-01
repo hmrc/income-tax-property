5f5493cc599e02567461ede642ed6b996fc7328a
Arun Kumar Madesh <56151567+arunkumar461@users.noreply.github.com>
2024-02-01 23:51:12 +0000

    Fix dependencies

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -9,16 +9,15 @@ object AppDependencies {
     "uk.gov.hmrc"                   %% "bootstrap-backend-play-30"  % bootstrapVersion,
     "uk.gov.hmrc.mongo"             %% "hmrc-mongo-play-30"         % hmrcMongoVersion,
     "com.fasterxml.jackson.module"  %% "jackson-module-scala"       % "2.14.2",
-    "org.typelevel"                 %% "cats-core"                  % "2.9.0"
   )
 
 
   val test: Seq[ModuleID] = Seq(
-    "uk.gov.hmrc"             %% "bootstrap-test-play-30"     % bootstrapVersion % Test,
+    "uk.gov.hmrc"             %% "bootstrap-test-play-30"     % bootstrapVersion              % Test,
     "uk.gov.hmrc.mongo"       %% "hmrc-mongo-test-play-30"    % hmrcMongoVersion              % Test,
     "org.scalamock"           %% "scalamock"                  % "5.2.0"                       % Test,
     "org.scalatest"           %% "scalatest"                  % "3.2.15"                      % Test,
     "com.vladsch.flexmark"    % "flexmark-all"                % "0.64.6"                      % Test,
-    "com.github.tomakehurst"  % "wiremock-jre8"               % "2.35.0"                      % Test
+    "com.github.tomakehurst"  % "wiremock-jre8-standalone"    % "2.35.1"                      % Test
   )
 }
