204c613f76057a28f0271152d3f897c06114dc67
vivekhmrc <vivek.kumar@digital.hmrc.gov.uk>
2024-03-28 03:05:07 +0000

    SASS-7511: Implement backend Create Annual property submission API

--- app/models/common/JourneyName.scala
+++ app/models/common/JourneyName.scala
@@ -29,4 +29,6 @@ object JourneyName extends Enum[JourneyName] with utils.PlayJsonEnum[JourneyName
 
   case object Income extends JourneyName("income")
 
+  case object AnnualPropertySubmission extends JourneyName("annual-property-submission")
+
 }
