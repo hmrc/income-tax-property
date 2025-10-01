aedff4786fedcea3a941eaa649e23a4354cdad2a
tbg2003 <72973046+tbg2003@users.noreply.github.com>
2025-10-01 14:54:28 +0100

    MIPR-1976: TEAM B - Upgrade dependencies

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,8 +2,8 @@ import sbt.*
 
 object AppDependencies {
 
-  private val bootstrapVersion = "10.1.0"
-  private val hmrcMongoVersion = "2.7.0"
+  private val bootstrapVersion = "10.2.0"
+  private val hmrcMongoVersion = "2.9.0"
   private val monocleVersion = "3.3.0"
   val jacksonAndPlayExclusions: Seq[ExclusionRule] = Seq(
     ExclusionRule(organization = "com.fasterxml.jackson.core"),
@@ -17,7 +17,7 @@ object AppDependencies {
     "uk.gov.hmrc"                  %% "bootstrap-backend-play-30" % bootstrapVersion,
     "uk.gov.hmrc.mongo"            %% "hmrc-mongo-play-30"        % hmrcMongoVersion,
     "org.typelevel"                %% "cats-core"                 % "2.13.0",
-    "com.fasterxml.jackson.module" %% "jackson-module-scala"      % "2.19.2",
+    "com.fasterxml.jackson.module" %% "jackson-module-scala"      % "2.20.0",
     "com.beachape"                 %% "enumeratum"                % "1.9.0",
     "com.beachape"                 %% "enumeratum-play-json"      % "1.9.0" excludeAll (jacksonAndPlayExclusions *),
     "dev.optics"                   %% "monocle-core"              % monocleVersion,
@@ -27,9 +27,9 @@ object AppDependencies {
   val test: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"           %% "bootstrap-test-play-30"   % bootstrapVersion,
     "uk.gov.hmrc.mongo"     %% "hmrc-mongo-test-play-30"  % hmrcMongoVersion,
-    "org.scalamock"         %% "scalamock"                % "7.4.1",
+    "org.scalamock"         %% "scalamock"                % "7.5.0",
     "org.scalatest"         %% "scalatest"                % "3.2.19",
-    "org.scalacheck"        %% "scalacheck"               % "1.18.1",
+    "org.scalacheck"        %% "scalacheck"               % "1.19.0",
     "org.scalatestplus"     %% "scalacheck-1-15"          % "3.2.11.0",
     "com.vladsch.flexmark"   % "flexmark-all"             % "0.64.8",
     "com.github.tomakehurst" % "wiremock-jre8-standalone" % "3.0.1"
